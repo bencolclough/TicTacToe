0113dee6c643139885f5c8a0e8f2fa13670660a3
bencolclough <ben@tourdust.com>
2020-05-07 08:45:55 +0100

    created ask_name method

--- game.rb
+++ game.rb
@@ -1,19 +1,36 @@
-class Player
-  def initialize(name, counter)
-    @name = name
-    @counter = counter
-    puts "welcome to tictactoe #{@name}, you are playing #{@counter}'s"
-  end
   
-end
+  board = [[1,2,3],[4,5,6],[7,8,9]]  
+  
+  def display_board(board)
+    puts "TIC-TAC-TOE"
+    p board[0]
+    p board[1]
+    p board[2]
+  end
+  display_board(board)
+  
+  class Player
+    def initialize(name, counter)
+      @name = name
+      @counter = counter
+      puts "welcome to TicTacToe #{@name}, you are playing #{@counter}'s"
+    end
+  end
 
-def get_players()
-    puts "Player 1, Enter Your Name"
-    name = gets.chomp
-    Player.new(name, "X")
-    puts "Player 2, Enter Your Name"
-    name = gets.chomp
-    Player.new(name, "O")
-end
+  def ask_name()
+    print "Enter Your Name: "
+    gets.chomp
+  end
 
-get_players()
+  def get_players()
+    print "Player 1, "
+    Player.new(ask_name(), "X")
+    print "Player 2, "
+    Player.new(ask_name(), "O")
+  end
+
+get_players
+
+
+
+
---
+++ test.rb
@@ -0,0 +1,12 @@
+
+module Printboard
+    def Printboard.printarrays(board)
+        p board[0]
+        p board[1]
+        p board[2]
+    end
+end
+
+
+board = [[1,2,3],[4,5,6],[7,8,9]]
+Printboard.printarrays(board)
